ddce40476da97c4d732ae7e682d50770aa2b7f87
shubh1996 <shubhmehta1996@gmail.com>
2017-03-06 21:44:17 +0530

    added js

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+
+$( "#e1" ).click(function() {
+	if($(window).width()<800)
+  alert( "e1" );
+		
+});
+$( "#e2" ).click(function() {
+	if($(window).width()<800)
+  alert( "e2" );
+	
+});
+$( "#e3" ).click(function() {
+	if($(window).width()<800)
+  alert( "e3" );
+	
+});
+$( "#e4" ).click(function() {
+	if($(window).width()<800)
+  alert( "e4" );
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
